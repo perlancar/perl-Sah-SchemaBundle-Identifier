c0ba4c0895b23d1a2bde2ff2ace488944b31a396
perlancar (on netbook-lenovo-x1c-i7gen10-jun2025) <perlancar@gmail.com>
2025-11-11 11:52:28 +0700

    Rename schemas: identifier::no_u{s,}, identifier::no_u{s,}_delim

--- lib/Sah/Schema/identifier/no_u_delim.pm
+++ lib/Sah/Schema/identifier/no_u_delim.pm
@@ -1,4 +1,4 @@
-package Sah::Schema::identifier::no_us;
+package Sah::Schema::identifier::no_u_delim;
 
 use strict;
 
@@ -8,23 +8,24 @@ use strict;
 # VERSION
 
 our $schema = [str => {
-    summary => 'A string that starts with [a-z] followed by zero of more [a-z0-9]',
+    summary => 'A string that starts with [a-z] followed by zero of more [a-z0-9_] and ends with [a-z0-9]',
     description => <<'MARKDOWN',
 
-This is a version of C<identifier> that does not allow underscores, everywhere.
+This is a version of C<identifier> that does not allow underscore delimiters.
+Underscore is still allowed in the middle.
 
 MARKDOWN
     min_len => 1,
-    match => qr/\A[A-Za-z][A-Za-z0-9]*\z/,
+    match => qr/\A(?:[A-Za-z]|[A-Za-z][A-Za-z0-9_]*[A-Za-z0-9])\z/,
     examples => [
         {value=>{}, valid=>0, summary=>"Not a string"},
         {value=>'', valid=>0, summary=>'Empty string'},
         {value=>'a', valid=>1},
         {value=>'a1', valid=>1},
         {value=>'Aa', valid=>1},
-        {value=>'a_', valid=>0, summary=>'Contains underscore'},
-        {value=>'_a', valid=>0, summary=>'Contains underscore'},
-        {value=>'a__b', valid=>0, summary=>'Contains underscore'},
+        {value=>'a_', valid=>0, summary=>'Contains underscore delimiter'},
+        {value=>'_a', valid=>0, summary=>'Contains underscore delimiter'},
+        {value=>'a__b', valid=>1},
         {value=>'a b', valid=>0, summary=>'Contains whitespace'},
         {value=>'ab-', valid=>0, summary=>'Contains invalid character'},
     ],
